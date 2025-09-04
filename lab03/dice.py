77dbcc9176b7b55aed66d3b517ab7069bfd97580
e-ycyang <e.yang4518@gmail.com>
2025-09-04 14:11:53 -0400

    Update dice.py

--- lab03/dice.py
+++ lab03/dice.py
@@ -1,12 +1,12 @@
 """A simple die roller
 
-
 Author: Ethan Yang, ey283
 Date: September 2, 2025"""
 
-first = 1
-last = 6
+first = int(input('Type the first number: '))
+last = int(input('Type the last number: '))
 
 from random import randint
 roll = randint(first,last) + randint(first,last)
-print('Choosing two numbers between '+str(first)+' and '+str(last))
+print('Choosing two numbers between '+str(first)+' and '+str(last)+'.')
+print('The sum is '+str(roll)+'.')
