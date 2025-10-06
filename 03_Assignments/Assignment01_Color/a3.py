515e275465bc68a4ec5e6288f693149f78aa57d8
e-ycyang <e.yang4518@gmail.com>
2025-10-06 15:04:29 -0400

    Organization + assignment 3

---
+++ 03_Assignments/Assignment01_Color/a3.py
@@ -0,0 +1,172 @@
+""" 
+Functions for Assignment A3
+
+This file contains the functions for the assignment. You should replace the 
+stubs with your own implementations.
+
+YOUR NAME(S) AND NETID(S) HERE
+DATE COMPLETED HERE
+"""
+import introcs
+import math
+
+
+def complement_rgb(rgb):
+    """
+    Returns the complement of color rgb.
+    
+    Parameter rgb: the color to complement
+    Precondition: rgb is an RGB object
+    """
+    # THIS IS WRONG. FIX IT
+    return introcs.RGB(rgb.red, rgb.green, rgb.blue)
+
+
+def str5(value):
+    """
+    Returns value as a string, but expanded or rounded to be exactly 5 characters.
+    
+    The decimal point counts as one of the five characters.
+   
+    Examples:
+        str5(1.3546)  is  '1.355'.
+        str5(21.9954) is  '22.00'.
+        str5(21.994)  is  '21.99'.
+        str5(130.59)  is  '130.6'.
+        str5(130.54)  is  '130.5'.
+        str5(1)       is  '1.000'.
+    
+    Parameter value: the number to conver to a 5 character string.
+    Precondition: value is a number (int or float), 0 <= value <= 360.
+    """
+    # Remember that the rounding takes place at a different place depending 
+    # on how big value is. Look at the examples in the specification.
+    pass
+
+
+def str5_cmyk(cmyk):
+    """
+    Returns the string representation of cmyk in the form "(C, M, Y, K)".
+    
+    In the output, each of C, M, Y, and K should be exactly 5 characters long.
+    Hence the output of this function is not the same as str(cmyk)
+    
+    Example: if str(cmyk) is 
+    
+          '(0.0,31.3725490196,31.3725490196,0.0)'
+    
+    then str5_cmyk(cmyk) is '(0.000, 31.37, 31.37, 0.000)'. Note the spaces 
+    after the commas. These must be there.
+    
+    Parameter cmyk: the color to convert to a string
+    Precondition: cmyk is an CMYK object.
+    """
+    pass
+
+
+def str5_hsv(hsv):
+    """
+    Returns the string representation of hsv in the form "(H, S, V)".
+    
+    In the output, each of H, S, and V should be exactly 5 characters long.
+    Hence the output of this function is not the same as str(hsv)
+    
+    Example: if str(hsv) is 
+    
+          '(0.0,0.313725490196,1.0)'
+    
+    then str5_hsv(hsv) is '(0.000, 0.314, 1.000)'. Note the spaces after the
+    commas. These must be there.
+    
+    Parameter hsv: the color to convert to a string
+    Precondition: hsv is an HSV object.
+    """
+    pass
+
+
+def rgb_to_cmyk(rgb):
+    """
+    Returns a CMYK object equivalent to rgb, with the most black possible.
+    
+    Formulae from https://www.rapidtables.com/convert/color/rgb-to-cmyk.html
+    
+    Parameter rgb: the color to convert to a CMYK object
+    Precondition: rgb is an RGB object
+    """
+    # The RGB numbers are in the range 0..255.
+    # Change them to the range 0..1 by dividing them by 255.0.
+    pass
+
+
+def cmyk_to_rgb(cmyk):
+    """
+    Returns an RGB object equivalent to cmyk
+    
+    Formulae from https://www.rapidtables.com/convert/color/cmyk-to-rgb.html
+   
+    Parameter cmyk: the color to convert to a RGB object
+    Precondition: cmyk is an CMYK object.
+    """
+    # The CMYK numbers are in the range 0.0..100.0. 
+    # Deal with them the same way as the RGB numbers in rgb_to_cmyk()
+    pass
+
+
+def rgb_to_hsv(rgb):
+    """
+    Return an HSV object equivalent to rgb
+    
+    Formulae from https://en.wikipedia.org/wiki/HSL_and_HSV
+   
+    Parameter hsv: the color to convert to a HSV object
+    Precondition: rgb is an RGB object
+    """
+    # The RGB numbers are in the range 0..255.
+    # Change them to range 0..1 by dividing them by 255.0.
+    pass
+
+
+def hsv_to_rgb(hsv):
+    """
+    Returns an RGB object equivalent to hsv
+    
+    Formulae from https://en.wikipedia.org/wiki/HSL_and_HSV
+    
+    Parameter hsv: the color to convert to a RGB object
+    Precondition: hsv is an HSV object.
+    """
+    pass
+
+
+def contrast_value(value,contrast):
+    """
+    Returns value adjusted to the "sawtooth curve" for the given contrast
+    
+    At contrast = 0.5, the curve is the normal line y = x, so value is 
+    unaffected. If contrast < 0.5, values are pulled closer together, with all 
+    values collapsing to 0.5 when contrast = 0. If contrast > 0, the values are 
+    pulled farther apart, with all values becoming 0 or 1 when contrast = 1.
+    
+    Parameter value: the value to adjust
+    Precondition: value is a float in 0..1
+    
+    Parameter contrast: the contrast amount (0.5 is no contrast)
+    Precondition: contrast is a float in 0..1
+    """
+    pass
+
+
+def contrast_rgb(rgb,contrast):
+    """
+    Applies the given contrast to the RGB object rgb
+    
+    This function is a PROCEDURE. It modifies rgb and has no return value. It 
+    should apply contrast_value to the red, blue, and green values.
+    
+    Parameter rgb: the color to adjust
+    Precondition: rgb is an RGB object
+    
+    Parameter contrast: the contrast amount (0.5 is no contrast)
+    Precondition: contrast is a float in 0..1
+    """
+    pass
---
+++ 03_Assignments/Assignment01_Color/a3test.py
@@ -0,0 +1,216 @@
+""" 
+Unit Test for Assignment A3
+
+This module implements several test cases for a3.  It is incomplete. You should 
+look though this file for places to add tests.
+
+YOUR NAME(S) AND NETID(S) HERE
+DATE COMPLETED HERE
+""" 
+import introcs
+import a3
+
+
+def test_complement():
+    """
+    Test function complement
+    """
+    print('Testing complement')
+    
+    # One test is really good enough here
+    comp = a3.complement_rgb(introcs.RGB(250, 0, 71))
+    introcs.assert_equals(255-250, comp.red)
+    introcs.assert_equals(255-0,   comp.green)
+    introcs.assert_equals(255-71,  comp.blue)
+    
+    # One more for good measure
+    comp = a3.complement_rgb(introcs.RGB(128, 64, 255))
+    introcs.assert_equals(255-128, comp.red)
+    introcs.assert_equals(255-64,  comp.green)
+    introcs.assert_equals(255-255, comp.blue)
+
+
+def test_str5():
+    """
+    Test function str5
+    """
+    introcs.assert_equals('130.6',  a3.str5(130.59))
+    introcs.assert_equals('130.5',  a3.str5(130.54))
+    introcs.assert_equals('100.0',  a3.str5(100))
+    introcs.assert_equals('100.6',  a3.str5(100.56))
+    introcs.assert_equals('99.57',  a3.str5(99.566))
+    introcs.assert_equals('99.99',  a3.str5(99.99))
+    introcs.assert_equals('100.0',  a3.str5(99.995))
+    introcs.assert_equals('22.00',  a3.str5(21.99575))
+    introcs.assert_equals('21.99',  a3.str5(21.994))
+    introcs.assert_equals('10.01',  a3.str5(10.013567))
+    introcs.assert_equals('10.00',  a3.str5(10.000000005))
+    introcs.assert_equals('10.00',  a3.str5(9.9999))
+    introcs.assert_equals('9.999',  a3.str5(9.9993))
+    introcs.assert_equals('1.355',  a3.str5(1.3546))
+    introcs.assert_equals('1.354',  a3.str5(1.3544))
+    introcs.assert_equals('0.046',  a3.str5(.0456))
+    introcs.assert_equals('0.045',  a3.str5(.0453))
+    introcs.assert_equals('0.006',  a3.str5(.0056))
+    introcs.assert_equals('0.001',  a3.str5(.0013))
+    introcs.assert_equals('0.000',  a3.str5(.0004))
+    introcs.assert_equals('0.001',  a3.str5(.0009999))
+    introcs.assert_equals('0.000',  a3.str5(1e-9))
+
+
+def test_str5_color():
+    """
+    Test the str5 functions for cmyk and hsv.
+    """
+    print('Testing str5_cmyk and str5_hsv')
+    
+    # Tests for str5_cmyk
+    # We need to make sure the coordinates round properly
+    text = a3.str5_cmyk(introcs.CMYK(98.448, 25.362, 72.8, 1.0))
+    introcs.assert_equals('(98.45, 25.36, 72.80, 1.000)',text)
+    
+    text = a3.str5_cmyk(introcs.CMYK(0.0, 1.5273, 100.0, 57.846))
+    introcs.assert_equals('(0.000, 1.527, 100.0, 57.85)',text)
+    
+    # Tests for str5_hsv (add two)
+
+
+def test_rgb_to_cmyk():
+    """
+    Test translation function rgb_to_cmyk
+    """
+    print('Testing rgb_to_cmyk')
+    
+    # The function should guarantee accuracy to three decimal places
+    rgb = introcs.RGB(255, 255, 255)
+    cmyk = a3.rgb_to_cmyk(rgb)
+    introcs.assert_equals(0.0, round(cmyk.cyan,3))
+    introcs.assert_equals(0.0, round(cmyk.magenta,3))
+    introcs.assert_equals(0.0, round(cmyk.yellow,3))
+    introcs.assert_equals(0.0, round(cmyk.black,3))
+    
+    rgb = introcs.RGB(0, 0, 0)
+    cmyk = a3.rgb_to_cmyk(rgb)
+    introcs.assert_equals(0.0, round(cmyk.cyan,3))
+    introcs.assert_equals(0.0, round(cmyk.magenta,3))
+    introcs.assert_equals(0.0, round(cmyk.yellow,3))
+    introcs.assert_equals(100.0, round(cmyk.black,3))
+        
+    rgb = introcs.RGB(217, 43, 164)
+    cmyk = a3.rgb_to_cmyk(rgb)
+    introcs.assert_equals(0.0, round(cmyk.cyan,3))
+    introcs.assert_equals(80.184, round(cmyk.magenta,3))
+    introcs.assert_equals(24.424, round(cmyk.yellow,3))
+    introcs.assert_equals(14.902, round(cmyk.black,3))
+
+
+def test_cmyk_to_rgb():
+    """
+    Test translation function cmyk_to_rgb
+    """
+    print('Testing cmyk_to_rgb')
+    # ADD TESTS TO ME
+
+
+def test_rgb_to_hsv():
+    """
+    Test translation function rgb_to_hsv
+    """
+    print('Testing rgb_to_hsv')
+    # ADD TESTS TO ME
+
+
+def test_hsv_to_rgb():
+    """
+    Test translation function hsv_to_rgb
+    """
+    print('Testing hsv_to_rgb')
+    # ADD TESTS TO ME
+
+
+def test_contrast_value():
+    """
+    Test translation function contrast_value
+    """
+    print('Testing contrast_value')
+    
+    # contrast == -1.0 (extreme)
+    result = a3.contrast_value(0.0,0.0)
+    introcs.assert_floats_equal(0.5,result)
+    
+    result = a3.contrast_value(1.0,0.0)
+    introcs.assert_floats_equal(0.5,result)
+    
+    # contrast < 0.5, bottom part of sawtooth
+    result = a3.contrast_value(0.1,0.25)
+    introcs.assert_floats_equal(0.3,result)
+    
+    # contrast < 0.5, middle of sawtooth
+    result = a3.contrast_value(0.4,0.3)
+    introcs.assert_floats_equal(0.4571429,result)
+    
+    # contrast < 0.5, upper part of sawtooth
+    result = a3.contrast_value(0.9,0.35)
+    introcs.assert_floats_equal(0.8142857,result)
+    
+    # contrast == 0.5, bottom part of sawtooth
+    result = a3.contrast_value(0.1,0.5)
+    introcs.assert_floats_equal(0.1,result)
+    
+    # contrast == 0.5, middle of sawtooth
+    result = a3.contrast_value(0.6,0.5)
+    introcs.assert_floats_equal(0.6,result)
+    
+    # contrast == 0.5, middle part of sawtooth
+    result = a3.contrast_value(0.9,0.5)
+    introcs.assert_floats_equal(0.9,result)
+    
+    # contrast > 0, upper part of sawtooth
+    result = a3.contrast_value(0.1,0.65)
+    introcs.assert_floats_equal(0.05384615,result)
+    
+    # contrast > 0, upper of sawtooth
+    result = a3.contrast_value(0.4,0.75)
+    introcs.assert_floats_equal(0.2,result)
+    
+    # contrast > 0, upper part of sawtooth
+    result = a3.contrast_value(0.9,0.7)
+    introcs.assert_floats_equal(0.95714286,result)
+    
+    # contrast == 1.0 (extreme)
+    result = a3.contrast_value(0.2,1.0)
+    introcs.assert_floats_equal(0.0,result)
+    
+    result = a3.contrast_value(0.6,1.0)
+    introcs.assert_floats_equal(1.0,result)
+
+
+def test_contrast_rgb():
+    """
+    Test translation function contrast_value
+    """
+    print('Testing contrast_rgb')
+    
+    # Darkening (less than 0.5) contrast
+    rgb = introcs.RGB(240, 15, 118)
+    hsv = a3.contrast_rgb(rgb,0.3)
+    introcs.assert_equals(220, rgb.red)
+    introcs.assert_equals(35,  rgb.green)
+    introcs.assert_equals(123, rgb.blue)
+    
+    # ADD TWO MORE TESTS
+
+
+# Script Code
+# THIS PREVENTS THE TESTS RUNNING ON IMPORT
+if __name__ == '__main__':
+    test_complement()
+    test_str5()
+    test_str5_color()
+    test_rgb_to_cmyk()
+    test_cmyk_to_rgb()
+    test_rgb_to_hsv()
+    test_hsv_to_rgb()
+    test_contrast_value()
+    test_contrast_rgb()
+    print('Module a3 passed all tests.')
